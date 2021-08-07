e931f40223aa8c9dc7ad2d88fcb37c48af941268
Pablo Nafarrate <pablo_nafa@hotmail.com>
2021-08-06 20:12:25 -0600

    general design of commit card

--- src/moduleCommits/lib/Card.tsx
+++ src/moduleCommits/lib/Card.tsx
@@ -1,16 +1,49 @@
 import React from 'react'
+import { formatDistanceToNow, parseISO } from 'date-fns'
 import { CommitDataFormat } from '../utils'
-import { Container } from './CardItems'
+import { Container, ImgGit } from './CardItems'
+import gitImg from 'img/git.png'
+
+const getDate = date => {
+  if (!date) {
+    return null
+  }
+
+  try {
+    return parseISO(date)
+  } catch (err) {
+    console.warn(err)
+    return null
+  }
+}
 
 export const Card: React.FC<CommitDataFormat> = ({ id, message, date, url, author }) => {
+  console.log(date)
   return (
     <Container>
-      <div>Mensaje: {message}</div>
-      <div className="flex">
-        <img src={author.avatar} alt="Avatar author" />
-        <div>
-          Autor: {author.name} - {author.user}
+      {/* <ImgGit src={gitImg} alt="" /> */}
+
+      <a href={url} className="font-semibold text-xl" target="_blank" rel="noreferrer">
+        {message}
+      </a>
+
+      <div className="flex items-center my-4">
+        <img className="w-10 h-10 rounded-full mr-4" src={author.avatar} alt="Avatar author" />
+        <div className="font-semibold">
+          Autor:{' '}
+          <a href={author.profileUrl} target="_blank" rel="noreferrer">
+            <span className="font-light" style={{ color: '#293462' }}>
+              {author.name} - {author.user}
+            </span>
+          </a>
         </div>
+      </div>
+
+      <div className="flex justify-between">
+        <span>
+          Fecha: {date} ({formatDistanceToNow(new Date(date), { includeSeconds: true })})
+        </span>
+        <span>{id}</span>
       </div>
     </Container>
   )
--- src/moduleCommits/lib/CardItems.tsx
+++ src/moduleCommits/lib/CardItems.tsx
@@ -1,5 +1,24 @@
 import { styled } from '@material-ui/styles'
 
 export const Container = styled('div')({
+  position: 'relative',
   margin: 8,
+  padding: '16px 28px',
+  overflow: 'hidden',
+  border: '1px solid #E6E5E5',
+  borderRadius: 10,
+  color: '#363636',
+  transition: 'box-shadow 0.3s ease-in-out',
+  '&:hover': {
+    boxShadow: '0px 0px 10px #00000033',
+  },
+})
+
+export const ImgGit = styled('img')({
+  position: 'absolute',
+  height: '100%',
+  width: 'auto',
+  left: -50,
+  top: 0,
+  opacity: 0.5,
 })
